7b0ae88301991ec2a7b30f16afb26b863aa8f4d3
unknown <rodolfhoazevedo@gmail.com>
2020-01-11 01:13:40 -0300

    adicao de um menu bottom-fixed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react';
+import './App.css'
+import { BottomNavigation, BottomNavigationAction } from '@material-ui/core';
+import { Restore, Favorite, LocationOn } from '@material-ui/icons';
 
 function App() {
+
+  const [value, setValue] = useState(0);
+
   return (
-    <div>Olá mundo</div>
+    <div className={'main_container'} >
+      <div>
+        <div className={'hero'}>
+          <h1>HERO</h1>
+        </div>
+        <section className={'section'}>
+          <h1>section of the main page</h1>
+        </section>
+      </div>
+      <BottomNavigation
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+        className={'botton_navigation_fixed'}
+        showLabels
+      >
+        <BottomNavigationAction label="Recents" icon={<Restore />} />
+        <BottomNavigationAction label="Favorite" icon={<Favorite />} />
+        <BottomNavigationAction label="Nearby" icon={<LocationOn />} />
+      </BottomNavigation>
+    </div>
   );
 }
 
